c177ced773d3f6c5ff6ebb2c44aca5af4cbeb527
ayadgu <ayadguillaume@gmail.com>
2024-03-05 17:37:58 +0100

    Masonry

--- src/components/AyadStudio/AydStudio.js
+++ src/components/AyadStudio/AydStudio.js
@@ -127,10 +127,14 @@ function AydStudio() {
           </p>
         </Col>
       </Row>
-      <br></br>
       <Row>
-        <ResponsiveMasonry columnsCountBreakPoints={{ 750: 2, 900: 3 }}>
-          {/* 350: 1, */}
+        <ResponsiveMasonry
+          columnsCountBreakPoints={{
+            350: 1,
+            750: 2,
+            900: 3,
+          }}
+        >
           <Masonry
             // columnsCount={{ xs: 1, sm: 2, md: 3 }}
             gutter="10px"
